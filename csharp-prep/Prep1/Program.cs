a88d63b4754820f32a69161769efaa695686937a
Embertheon <whi22060@byui.edu>
2025-04-26 20:53:55 -0600

    Update Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string First_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string Last_name = Console.ReadLine();
+
+        Console.Write($"Your name is {Last_name}, {First_name} {Last_name}"); 
     }
 }
