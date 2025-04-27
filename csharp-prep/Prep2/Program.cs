8f12158b606115dab5bef0fcff5aabe1e4e17a0d
Embertheon <whi22060@byui.edu>
2025-04-26 21:32:30 -0600

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter a percentage. %");
+        string number = Console.ReadLine();
+        int givenPercentage = int.Parse(number);
+         
+        if (givenPercentage >= 90){
+            Console.WriteLine("You got an A");
+        }
+        else if (givenPercentage >= 80){
+            Console.WriteLine("You got a B");
+        }
+        else if (givenPercentage >= 70){
+            Console.WriteLine("You got a C");
+        }
+        else if (givenPercentage >= 60){
+            Console.WriteLine("You got a D");
+        }
+        else{
+            Console.WriteLine("You got a F");
+        }
     }
 }
